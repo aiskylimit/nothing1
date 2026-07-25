a53353c45d777b922e6a1b988f5d72ffba9efa5b
phongdq25 <nothing>
2026-07-25 22:34:48 +0700

    update

--- anti-distill/project_commands.sh
+++ anti-distill/project_commands.sh
@@ -7,9 +7,9 @@ hf download VoCuc/anti-data \
 unzip -o anti_data.zip
 
 
-bash nothing1/anti-distill/script/train/train_teacher_lora.sh &
-bash nothing1/anti-distill/script/train/train_no_virtual_ascent.sh &
-bash nothing1/anti-distill/script/train/train_no_loss_base.sh &
-bash nothing1/anti-distill/script/train/train_no_conflict.sh &
+bash ./script/train/train_teacher_lora.sh &
+bash ./script/train/train_no_virtual_ascent.sh &
+bash ./script/train/train_no_loss_base.sh &
+bash ./script/train/train_no_conflict.sh &
 
 wait
