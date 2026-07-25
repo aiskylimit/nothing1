d8861bceed4bfadbc892613dc25868ca4695bb35
phongdq25 <nothing>
2026-07-25 23:07:50 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#1 +60
+#2
 #anti-distill
 #v1
 
@@ -16,8 +16,8 @@
 # make CUDAPATH=/usr/local/cuda-13.0
 # ./gpu_burn 36000000000
 
-kill -9 $(nvidia-smi --query-compute-apps=pid --format=csv,noheader)
-sleep 10
+# kill -9 $(nvidia-smi --query-compute-apps=pid --format=csv,noheader)
+# sleep 10
 nvidia-smi
 
 source ~/miniconda3/etc/profile.d/conda.sh
@@ -25,5 +25,5 @@ conda activate base
 export PATH=/usr/local/cuda/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
-cd ./anti-distill
-bash ./project_commands.sh
+# cd ./anti-distill
+# bash ./project_commands.sh
