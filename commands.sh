ee8209878cfac040926938e1f91f6043a9a7187d
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-07-27 17:44:13 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#2 -f-/home/ubuntu/aiskylimit_nothing1/anti-distill/experiments_gsm8k_7/traces/eval_teacher_lora_tau1.26_delta0.0/,/home/ubuntu/aiskylimit_nothing1/anti-distill/experiments_gsm8k_7/traces/eval_teacher_lora_tau1.5_delta0.0/ +a
+#2 -f-/home/ubuntu/aiskylimit_nothing1/experiments_gsm8k/traces/eval_teacher_lora_tau0.5_delta0.25/,/home/ubuntu/aiskylimit_nothing1/experiments_gsm8k/traces/eval_teacher_lora_tau0.5_delta0.8/ +a
 #doge
 #v2
 
