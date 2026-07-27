402b4182ac691cd0339cc36c7ae9f1f17261febe
phongdq25 <nothing>
2026-07-27 12:24:27 +0700

    update

--- anti-distill/script/eval/gpqa/run_eval_2.sh
+++ anti-distill/script/eval/gpqa/run_eval_2.sh
@@ -17,13 +17,13 @@ for pair in "${pairs[@]}"; do
     echo " START PIPELINE WITH DELTA = ${delta} AND TAU = ${tau}"
     echo "=========================================================================="
 
-    # echo ">>> [1/4] Eval Teacher with delta = ${delta} and tau = ${tau}..."
-    # accelerate launch --config_file acc_config_2.yaml --main_process_port 0 "$gen"  \
-    # hydra.run.dir=experiments_gpqa_cala/metadata/eval/lora_teacher_tau${tau}_delta${delta} \
-    # is_teacher=true exp_dir=experiments_gpqa_cala answer_force=true tau=${tau} \
-    # data_split=gpqa_test batch_size=256 trace_name=eval_teacher_lora_tau${tau}_delta${delta} seed=42 \
-    # tokenizer=deepseek-ai/DeepSeek-R1-Distill-Qwen-7B \
-    # teacher=${teacher} delta=${delta} max_samples=2880 max_length=1024 max_prompt_length=256
+    echo ">>> [1/4] Eval Teacher with delta = ${delta} and tau = ${tau}..."
+    accelerate launch --config_file acc_config_2.yaml --main_process_port 0 "$gen"  \
+    hydra.run.dir=experiments_gpqa_cala/metadata/eval/lora_teacher_tau${tau}_delta${delta} \
+    is_teacher=true exp_dir=experiments_gpqa_cala answer_force=true tau=${tau} \
+    data_split=gpqa_test batch_size=128 trace_name=eval_teacher_lora_tau${tau}_delta${delta} seed=42 \
+    tokenizer=deepseek-ai/DeepSeek-R1-Distill-Qwen-7B \
+    teacher=${teacher} delta=${delta} max_samples=2880 max_length=1024 max_prompt_length=256
 
     # echo ">>> [2/4] Gen Training Traces with delta = ${delta} and tau = ${tau}..."
     # python "$gen" \
@@ -37,22 +37,22 @@ for pair in "${pairs[@]}"; do
     # answer_force=true \
     # tau=${tau} seed=42 delta=${delta} max_samples=2880 
 
-    echo ">>> [3/4] Training Student model with delta = ${delta} and tau = ${tau}..."
-    accelerate launch --config_file acc_config_2.yaml --main_process_port 0 distill.py \
-    hydra.run.dir=experiments_gpqa_cala/metadata/distill/lora_teacher \
-    student=meta-llama/Llama-3.2-3B \
-    tokenizer=meta-llama/Llama-3.2-3B-Instruct \
-    exp_dir=experiments_gpqa_cala \
-    train_traces=experiments_gpqa_cala/traces_mmlu/teacher_lora_tau${tau}_delta${delta} \
-    holdout_traces=traces_holdout per_device_batch_size=4 \
-    model_name=student_manua_tau${tau}_delta${delta}
+    # echo ">>> [3/4] Training Student model with delta = ${delta} and tau = ${tau}..."
+    # accelerate launch --config_file acc_config_2.yaml --main_process_port 0 distill.py \
+    # hydra.run.dir=experiments_gpqa_cala/metadata/distill/lora_teacher \
+    # student=meta-llama/Llama-3.2-3B \
+    # tokenizer=meta-llama/Llama-3.2-3B-Instruct \
+    # exp_dir=experiments_gpqa_cala \
+    # train_traces=experiments_gpqa_cala/traces_mmlu/teacher_lora_tau${tau}_delta${delta} \
+    # holdout_traces=traces_holdout per_device_batch_size=4 \
+    # model_name=student_manua_tau${tau}_delta${delta}
 
     echo ">>> [4/4] Eval Student model with delta = ${delta} and tau = ${tau}..."
     accelerate launch --config_file acc_config_2.yaml --main_process_port 0 student_eval.py  \
     hydra.run.dir=experiments_gpqa_cala/metadata/eval/student_mmlu \
     teacher=experiments_gpqa_cala/models/student_manua_tau${tau}_delta${delta}/final \
     is_teacher=false exp_dir=experiments_gpqa_cala answer_force=true \
-    data_split=gpqa_test batch_size=256 max_samples=2880 \
+    data_split=gpqa_test batch_size=320 max_samples=2880 \
     trace_name=eval_student_mmlu_tau${tau}_delta${delta} seed=42 max_length=1024 max_prompt_length=256
 
 done
