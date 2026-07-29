af67dcfd9cc89f75456483f544821a0544f6e0e6
phongdq25 <nothing>
2026-07-29 23:05:34 +0700

    update

--- DOGe/project_commands.sh
+++ DOGe/project_commands.sh
@@ -8,9 +8,9 @@ export WANDB_DISABLED=True
 
 mkdir -p data/r1-qwen-7b
 
-# wget -c \
-#   "https://huggingface.co/datasets/VoCuc/anti-data/resolve/main/distillation_data.jsonl?download=true" \
-#   -O data/r1-qwen-7b/distillation_data.jsonl
+wget -c \
+  "https://huggingface.co/datasets/VoCuc/anti-data/resolve/main/distillation_data.jsonl?download=true" \
+  -O data/r1-qwen-7b/distillation_data.jsonl
 
 export LD_LIBRARY_PATH=$PWD/.venv/lib/python3.10/site-packages/nvidia/cusparselt/lib:$LD_LIBRARY_PATH
 
