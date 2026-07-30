7096c906d330573b72397700b865966f14fa4259
phongdq25 <nothing>
2026-07-30 21:48:50 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#2 -0-10
+#1 +120
 #contra
 #v1
 
@@ -43,6 +43,6 @@ export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 # cd contra-velocity-field-design
 # bash ./project_commands.sh
 
-# cd regkd-vlm2vec
-# bash ./project_commands.sh
+cd regkd-vlm2vec
+bash ./project_commands.sh
 
